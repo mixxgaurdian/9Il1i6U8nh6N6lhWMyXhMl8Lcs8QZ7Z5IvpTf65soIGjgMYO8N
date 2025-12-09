34f2f25752bb759381a2ae826cac742b9fcef47e
mixxgaurdian <kylen09flanagan@gmail.com>
2025-12-08 20:27:32 -0600

    Forge/ and prison life support added loadstring

--- loadstring.lua
+++ loadstring.lua
@@ -5,4 +5,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/mixxgaurdian/9Il1i6U8
 --scripts--
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mixxgaurdian/9Il1i6U8nh6N6lhWMyXhMl8Lcs8QZ7Z5IvpTf65soIGjgMYO8N/refs/heads/main/xkJ8rtl85wxTZ9apvlfQPx7U88r5E3WVZGMoP.lua"))()
 
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mixxgaurdian/9Il1i6U8nh6N6lhWMyXhMl8Lcs8QZ7Z5IvpTf65soIGjgMYO8N/refs/heads/main/LOK83u70UdGWBhj3LwexaiKVy5Q8MJTrxhM6KUz.lua"))()
