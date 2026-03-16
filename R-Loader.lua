b245109fd1c49f6916dc43ffb19f67abd19aaf52
mixxgaurdian <kylen09flanagan@gmail.com>
2026-03-16 18:07:53 -0500

    tag fix 2

--- R-Loader.lua
+++ R-Loader.lua
@@ -866,7 +866,7 @@ local FullCatalog = {
         {
             Name = "R-Loader | Rivals Demo",
             Icon = BG_christmas_1,
-            Description = "R-Loader version for Rivals with aimbot and esp",
+            Description = "R-Loader version for Rivals with aimbot and esp.",
             Load = [[
                 getgenv().autoload = autoloadEnabled
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/mixxgaurdian/9Il1i6U8nh6N6lhWMyXhMl8Lcs8QZ7Z5IvpTf65soIGjgMYO8N/refs/heads/main/scripts/R-Loader-aimDemo.lua", true))()
